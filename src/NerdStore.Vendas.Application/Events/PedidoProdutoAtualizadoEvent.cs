07acdfe1599eb6f193433194113c657a97d13e59
Davi Freitas <davi_carvalho1@live.com>
2022-05-18 09:31:39 -0300

    Feat: add novos commands de crud

--- src/NerdStore.Vendas.Application/Events/PedidoProdutoAtualizadoEvent.cs
+++ src/NerdStore.Vendas.Application/Events/PedidoProdutoAtualizadoEvent.cs
@@ -3,24 +3,19 @@ using System;
 
 namespace NerdStore.Vendas.Application.Events
 {
-    public class PedidoItemPedidoAdicionadoEvent : Event
+    public class PedidoProdutoAtualizadoEvent : Event
     {
         public Guid ClienteId { get; private set; }
         public Guid PedidoId { get; private set; }
         public Guid ProdutoId { get; private set; }
-        public string ProdutoNome { get; private set; }
-        public decimal ValorUnitario { get; private set; }
         public int Quantidade { get; private set; }
 
-        public PedidoItemPedidoAdicionadoEvent(Guid clienteId, Guid pedidoId, Guid produtoId,
-                                               string produtoNome, decimal valorUnitario, int quantidade)
+        public PedidoProdutoAtualizadoEvent(Guid clienteId, Guid pedidoId, Guid produtoId, int quantidade)
         {
             AggregateId = pedidoId;
             ClienteId = clienteId;
             PedidoId = pedidoId;
             ProdutoId = produtoId;
-            ProdutoNome = produtoNome;
-            ValorUnitario = valorUnitario;
             Quantidade = quantidade;
         }
     }
--- src/NerdStore.WebApp.Mvc/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.Mvc/Setup/DependencyInjection.cs
@@ -8,6 +8,8 @@ using NerdStore.Catalogo.Domain.Events;
 using NerdStore.Core.Communication.Mediator;
 using NerdStore.Core.Messages.CommonMessages.Notifications;
 using NerdStore.Vendas.Application.Commands;
+using NerdStore.Vendas.Application.Events;
+using NerdStore.Vendas.Application.Queries;
 using NerdStore.Vendas.Data;
 using NerdStore.Vendas.Data.Repository;
 using NerdStore.Vendas.Domain;
@@ -34,9 +36,17 @@ namespace NerdStore.WebApp.Mvc.Setup
 
             // Vendas
             services.AddScoped<IPedidoRepository, PedidoRepository>();
+            services.AddScoped<IPedidoQueries, PedidoQueries>();
             services.AddScoped<VendasContext>();
 
             services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
+            services.AddScoped<IRequestHandler<AtualizarItemPedidoCommand, bool>, PedidoCommandHandler>();
+            services.AddScoped<IRequestHandler<RemoverItemPedidoCommand, bool>, PedidoCommandHandler>();
+            services.AddScoped<IRequestHandler<AplicarVoucherPedidoCommand, bool>, PedidoCommandHandler>();
+
+            services.AddScoped<INotificationHandler<PedidoRascunhoIniciadoEvent>, PedidoEventHandler>();
+            services.AddScoped<INotificationHandler<PedidoAtualizadoEvent>, PedidoEventHandler>();
+            services.AddScoped<INotificationHandler<PedidoItemPedidoAdicionadoEvent>, PedidoEventHandler>();
         }
     }
 }
